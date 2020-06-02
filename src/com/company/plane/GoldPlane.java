48fead940b0d7c124c9d0771b560974c0e53edfc
Gonzalo Lascar <44815702+gonzalas@users.noreply.github.com>
2020-06-02 19:28:27 -0300

    Modifing plane classes

--- src/com/company/plane/GoldPlane.java
+++ src/com/company/plane/GoldPlane.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/com/company/plane/GoldPlane.java
 package com.company.plane;
-=======
-package com.company.Plane;
->>>>>>> 45e7a8e75faae606199933519b7965af82e3411c:src/com/company/Plane/GoldPlane.java
 
 public class GoldPlane extends Plane {
     private Boolean catering;
